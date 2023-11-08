880faaedaaa5a314317bd3f50ab4d9e1bcae4a95
Priyalatha <priyalatha24042002@gmail.com>
2023-11-08 11:30:04 +0530

    space added

--- src/MaincallMethod/Mainmethod.java
+++ src/MaincallMethod/Mainmethod.java
@@ -8,6 +8,7 @@ public class Mainmethod {
 	public static void main(String[]args) {
 		int arr[] ={17,21,15,13,5,19,16};	
 		Primeclass obj = new Primeclass ();
+		
 		System.out.println("The prime numbers are : "+obj.primenumber(arr));
 	}
 	
