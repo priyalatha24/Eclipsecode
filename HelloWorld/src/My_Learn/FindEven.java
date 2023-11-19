45406c923834b859cf7222fedf1125cb57ca7a41
Priyalatha <priyalatha24042002@gmail.com>
2023-11-19 08:41:16 +0530

    even number

---
+++ HelloWorld/src/My_Learn/FindEven.java
@@ -0,0 +1,15 @@
+package My_Learn;
+
+import java.util.ArrayList;
+
+public class FindEven {
+	public ArrayList<Integer> even(int a[]){
+		ArrayList<Integer> E= new ArrayList<Integer>();
+		for(int i=0;i<a.length;i++) {
+			if(a[i]%2==0)
+				E.add(a[i]);
+				}
+		return E;
+     }
+	
+}
