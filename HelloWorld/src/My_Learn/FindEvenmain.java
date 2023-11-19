dccc867168721278f266fb9488f24596325c7dfa
Priyalatha <priyalatha24042002@gmail.com>
2023-11-19 08:41:45 +0530

    even main first commit

--- HelloWorld/src/My_Learn/FindEvenmain.java
+++ HelloWorld/src/My_Learn/FindEvenmain.java
@@ -1,10 +1,21 @@
 package My_Learn;
 
+import java.util.Scanner;
+
 public class FindEvenmain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner s= new Scanner(System.in);
+		int n= s.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+			a[i]=s.nextInt();
+		FindEven obj =new FindEven();
+		System.out.println("The Even numbers :"+obj.even(a));
+		
 
 	}
 
-}
+	}
+
+
