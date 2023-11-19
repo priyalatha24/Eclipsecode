16ab8770b82e9c34d52c2dd20683447dd2b4d0e4
Priyalatha <priyalatha24042002@gmail.com>
2023-11-19 08:55:29 +0530

    MyLearn

---
+++ HelloWorld/src/My_Learn/if_else.java
@@ -0,0 +1,16 @@
+package My_Learn;
+
+public class if_else {
+
+	    public String compareNM(int n,int m){
+	        // code here
+	        if(n<m){
+	            return "lesser";
+	        }if else(n==m){
+	            return "equal";
+	        }else(n>m){
+	            return "greater";
+	        }
+	    }
+	}
+
